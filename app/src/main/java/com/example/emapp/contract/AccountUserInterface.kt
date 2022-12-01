d73ccfca8eb940b1dc77bd55be04ab225abc5690
Ash <emirmedzitov@gmail.com>
2022-12-01 20:04:02 +0300

    version 2.1

--- app/src/main/java/com/example/emapp/contract/AccountUserInterface.kt
+++ app/src/main/java/com/example/emapp/contract/AccountUserInterface.kt
@@ -1,10 +1,13 @@
 package com.example.emapp.contract
 
 import android.widget.EditText
+import androidx.fragment.app.Fragment
 
 interface AccountUserInterface {
     interface View {
         fun initView()
+        fun replaceFragment(fragment: Fragment)
+        fun logOut()
     }
 
     interface Presenter {
